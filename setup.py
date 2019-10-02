5566f95ad9613d46c76a18ac970a6ecadf3ea0f3
Aid Ahmetovic <a.ahmetovic@netconomy.net>
2019-10-02 15:50:01 +0200

    Added README file as well as initial setup.py (not tested)

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as file:
+    description = file.read()
+
+setup(name='py_openthesaurus',
+      version='1.0.0',
+      description='Get synonyms of an input word (German language)',
+      long_description=description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Machine Learning',
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Natural Language Processing :: German'],
+      url='https://github.com/Aid91/py_openthesaurus',
+      author='Aid91',
+      author_email='aidahmetovic91@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=['tests*']),
+      install_requires=["translate"],
+      entry_points={"console_scripts": ['py_openthesaurus=py_openthesaurus.main:main']},
+      zip_safe=False)
